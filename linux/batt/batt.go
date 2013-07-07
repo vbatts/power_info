3a818bc47580e40a4349d3865c1a2a00fb96787b
Vincent Batts <vbatts@redhat.com>
2013-07-07 10:30:36 -0400

    info collection for battery percentage. Now to get the testing reproducable and then start working on EMA rate of usage, to see an expected life.

--- linux/batt/batt.go
+++ linux/batt/batt.go
@@ -4,12 +4,53 @@ import (
 	"github.com/vbatts/power_info/linux"
 	"path/filepath"
 	"strings"
+  "strconv"
 )
 
 type Battery struct {
 	Key string
 }
 
+func (b *Battery) Status() string {
+  str, _ := linux.StringFromFile(psp(b.Key, "status"))
+  return str
+}
+
+func (b *Battery) ChargeNow() int64 {
+  var str string
+  if linux.IsFile(psp(b.Key, "energy_now")) {
+    str, _ = linux.StringFromFile(psp(b.Key, "energy_now"))
+  } else {
+    str, _ = linux.StringFromFile(psp(b.Key, "charge_now"))
+  }
+  i, _ := strconv.ParseInt(str, 10, 64)
+  return i
+}
+
+func (b *Battery) ChargeFull() int64 {
+  var str string
+  if linux.IsFile(psp(b.Key, "energy_full")) {
+    str, _ = linux.StringFromFile(psp(b.Key, "energy_full"))
+  } else {
+    str, _ = linux.StringFromFile(psp(b.Key, "charge_full"))
+  }
+  i, _ := strconv.ParseInt(str, 10, 64)
+  return i
+}
+
+func (b *Battery) Percent() float64 {
+  return float64(b.ChargeNow()) / float64(b.ChargeFull())
+}
+
+// given the Battery.Key, get the path for this power_supply
+func psp(key, value string) string {
+	if len(value) == 0 {
+		return filepath.Join(linux.PowerSupplyPath, key)
+	}
+	return filepath.Join(linux.PowerSupplyPath, key, value)
+}
+
+// Get an Array of the Batteries available on this system
 func GetBatteries() (batts []Battery, err error) {
 	possibilities, err := filepath.Glob(linux.PowerSupplyPath + "*/type")
 	if err != nil {
--- linux/batt/batt_test.go
+++ linux/batt/batt_test.go
@@ -5,10 +5,25 @@ import (
   "fmt"
 )
 
+func TestFuncs(t *testing.T) {
+  str := psp("BAT0","")
+  fmt.Println(str)
+
+  str = psp("BAT0","status")
+  fmt.Println(str)
+
+}
+
 func TestBatts(t *testing.T) {
   batts ,err := GetBatteries()
   if err != nil {
     t.Error(err)
   }
-  fmt.Println(batts)
+  for _, batt := range batts {
+    fmt.Println(batt.Status())
+    fmt.Println(batt.ChargeNow())
+    fmt.Println(batt.ChargeFull())
+    fmt.Println(batt.Percent())
+  }
+
 }
