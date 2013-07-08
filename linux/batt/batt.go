61426b818c78753012ad65498c6e7c766279ca2e
Vincent Batts <vbatts@redhat.com>
2013-07-08 10:52:41 -0400

    more methods

--- linux/batt/batt.go
+++ linux/batt/batt.go
@@ -3,8 +3,9 @@ package batt
 import (
 	"github.com/vbatts/power_info/linux"
 	"path/filepath"
+	"strconv"
 	"strings"
-  "strconv"
+	"time"
 )
 
 type Battery struct {
@@ -12,34 +13,57 @@ type Battery struct {
 }
 
 func (b *Battery) Status() string {
-  str, _ := linux.StringFromFile(psp(b.Key, "status"))
-  return str
+	str, _ := linux.StringFromFile(psp(b.Key, "status"))
+	return str
 }
 
 func (b *Battery) ChargeNow() int64 {
-  var str string
-  if linux.IsFile(psp(b.Key, "energy_now")) {
-    str, _ = linux.StringFromFile(psp(b.Key, "energy_now"))
-  } else {
-    str, _ = linux.StringFromFile(psp(b.Key, "charge_now"))
-  }
-  i, _ := strconv.ParseInt(str, 10, 64)
-  return i
+	var str string
+	if linux.IsFile(psp(b.Key, "energy_now")) {
+		str, _ = linux.StringFromFile(psp(b.Key, "energy_now"))
+	} else {
+		str, _ = linux.StringFromFile(psp(b.Key, "charge_now"))
+	}
+	i, _ := strconv.ParseInt(str, 10, 64)
+	return i
 }
 
 func (b *Battery) ChargeFull() int64 {
-  var str string
-  if linux.IsFile(psp(b.Key, "energy_full")) {
-    str, _ = linux.StringFromFile(psp(b.Key, "energy_full"))
-  } else {
-    str, _ = linux.StringFromFile(psp(b.Key, "charge_full"))
-  }
-  i, _ := strconv.ParseInt(str, 10, 64)
-  return i
+	var str string
+	if linux.IsFile(psp(b.Key, "energy_full")) {
+		str, _ = linux.StringFromFile(psp(b.Key, "energy_full"))
+	} else {
+		str, _ = linux.StringFromFile(psp(b.Key, "charge_full"))
+	}
+	i, _ := strconv.ParseInt(str, 10, 64)
+	return i
 }
 
 func (b *Battery) Percent() float64 {
-  return float64(b.ChargeNow()) / float64(b.ChargeFull())
+	return float64(b.ChargeNow()) / float64(b.ChargeFull())
+}
+
+func (b *Battery) GetInfo() (*linux.Info, error) {
+	info := linux.Info{
+		Key:     b.Key,
+		Time:    time.Now().UnixNano(),
+		Values:  map[string]string{},
+		Load:    linux.GetLoadAvg(),
+		Version: linux.GetVersion(),
+	}
+	files, err := filepath.Glob(psp(b.Key, "*"))
+	if err != nil {
+		return nil, err
+	}
+	for _, file := range files {
+		if linux.IsFile(file) {
+			if str, err := linux.StringFromFile(file); err == nil {
+				info.Values[filepath.Base(file)] = str
+			}
+		}
+	}
+
+	return &info, nil
 }
 
 // given the Battery.Key, get the path for this power_supply
@@ -50,6 +74,17 @@ func psp(key, value string) string {
 	return filepath.Join(linux.PowerSupplyPath, key, value)
 }
 
+/*
+Given a key like "AC" or "BAT0", determine whether it is a battery type
+*/
+func IsBattery(key string) bool {
+	type_file := filepath.Join(linux.PowerSupplyPath, key, "type")
+	if str, err := linux.StringFromFile(type_file); err == nil && strings.Contains(str, "Battery") {
+		return true
+	}
+	return false
+}
+
 // Get an Array of the Batteries available on this system
 func GetBatteries() (batts []Battery, err error) {
 	possibilities, err := filepath.Glob(linux.PowerSupplyPath + "*/type")
@@ -57,9 +92,8 @@ func GetBatteries() (batts []Battery, err error) {
 		return batts, err
 	}
 	for _, poss := range possibilities {
-		str, _ := linux.StringFromFile(poss)
-		if strings.Contains(str, "Battery") {
-			key := filepath.Base(filepath.Dir(poss))
+		key := filepath.Base(filepath.Dir(poss))
+		if linux.IsBattery(key) {
 			batts = append(batts, Battery{Key: key})
 		}
 	}
--- linux/batt/batt_test.go
+++ linux/batt/batt_test.go
@@ -1,29 +1,35 @@
 package batt
 
 import (
-  "testing"
-  "fmt"
+	"fmt"
+	"testing"
 )
 
 func TestFuncs(t *testing.T) {
-  str := psp("BAT0","")
-  fmt.Println(str)
+	str := psp("BAT0", "")
+	fmt.Println(str)
 
-  str = psp("BAT0","status")
-  fmt.Println(str)
+	str = psp("BAT0", "status")
+	fmt.Println(str)
+
+	if IsBattery("AC") {
+		t.Errorf("AC should not be seen as a battery")
+	}
 
 }
 
+// figure out how to test this
 func TestBatts(t *testing.T) {
-  batts ,err := GetBatteries()
-  if err != nil {
-    t.Error(err)
-  }
-  for _, batt := range batts {
-    fmt.Println(batt.Status())
-    fmt.Println(batt.ChargeNow())
-    fmt.Println(batt.ChargeFull())
-    fmt.Println(batt.Percent())
-  }
+	batts, err := GetBatteries()
+	if err != nil {
+		t.Error(err)
+	}
+	for _, batt := range batts {
+		fmt.Println(batt.Status())
+		fmt.Println(batt.ChargeNow())
+		fmt.Println(batt.ChargeFull())
+		fmt.Println(batt.Percent())
+		fmt.Println(batt.GetInfo())
+	}
 
 }
